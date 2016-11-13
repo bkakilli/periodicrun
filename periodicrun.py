4e2231c48e62728b58abce12edaf0eeb64860a6e
Burak Kakillioğlu <bkakillioglu@gmail.com>
2016-11-12 21:16:10 -0500

    Update periodicrun.py

--- periodicrun.py
+++ periodicrun.py
@@ -1,3 +1,37 @@
+'''
+MIT License
+
+Copyright (c) 2016 Burak Kakillioğlu
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+'''
+Author: Burak Kakillioglu
+Date: 11/12/2016
+Library name: periodicrun
+Description:
+Python library for running a loop periodically with ~100 microseconds accuracy.
+This library provides functionality for periodic execution of a method with accurate timing.
+It is meant by accurate timing that periodicrun guaranties that the loop will run every x seconds with 100 microsends (default) accuracy.
+periodicrun uses Python's built-in sched library to schedule the execution of the given method periodically.
+'''
+
 import sched, time, threading
 
 class periodicrun:
